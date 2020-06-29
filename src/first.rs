fa61ada38420043ab74704f0274e392395b3dd7a
masadl <mju546mist@gmail.com>
2020-06-29 23:47:38 +0900

    fix typo

--- src/first.rs
+++ src/first.rs
@@ -6,6 +6,6 @@ fn sub_sort(x: &mut[u32], up: bool) {
     unimplemented!();
 }
 
-fun compare_and_swap(x: &mut[u32], up: bool) {
-    unimplemented!():
+fn compare_and_swap(x: &mut[u32], up: bool) {
+    unimplemented!();
 }
