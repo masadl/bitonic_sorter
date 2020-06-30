cb17286cbb283d749ea80a114b911fce82359e00
masadl <mju546mist@gmail.com>
2020-06-30 23:49:17 +0900

    feat: impl of compare_and_swap

--- src/first.rs
+++ src/first.rs
@@ -17,5 +17,12 @@ fn sub_sort(x: &mut[u32], up: bool) {
 }
 
 fn compare_and_swap(x: &mut[u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        for i in 0..mid_point {
+            if (x[i] > x[mid_point + i]) == up {
+                x.swap(i, mid_point + i);
+            } 
+        }
+    }
 }
